7fcee7c7958225d54c693dfbe54435388c7264b0
hohoTT <609029365@qq.com>
2016-05-18 19:39:21 +0800

    添加一个有关string的测试类

---
+++ test/src/com/wt/test/StringEqualTest.java
@@ -0,0 +1,15 @@
+package com.wt.test;
+
+public class StringEqualTest {
+
+	public static void main(String[] args) {
+        String s1 = "Programming";
+        String s2 = new String("Programming");
+        String s3 = "Program" + "ming";
+        
+        System.out.println(s1 == s2);
+        System.out.println(s1 == s3);
+        System.out.println(s1 == s1.intern());
+    }
+	
+}
